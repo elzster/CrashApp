8473f5478d2dfe0019e06136dbf3c92b8de16f41
elzster <hakim.elie1@gmail.com>
2019-12-22 10:11:20 -0500

    Json Date Parser

--- nycproject/static/js/bargraph.js
+++ nycproject/static/js/bargraph.js
@@ -12,6 +12,43 @@ var nycData = `/datafile/`;
 //     row.text(`${key}:${value}`)
 //   })
 // });
+
+
+//Functions to Translate all JSON dates into usable dates.
+// var MyDate_String_Value = "/Date(1414972800000)/"
+// var value = new Date
+//             (
+//                  parseInt(MyDate_String_Value.replace(/(^.*\()|([+-].*$)/g, ''))
+//             );
+// var dat = value.getMonth() + 
+//                          1 + 
+//                        "/" + 
+//            value.getDate() + 
+//                        "/" + 
+//        value.getFullYear();
+// alert(dat);
+
+
+//console log of all data within JSon Object to use.
 d3.json(nycData).then( data =>{
-  console.log(data);
+  //to parse the numeric data from string
+  console.log(data[0]);
+  //latitude longitude
+  console.log(parseFloat(data[0].latitude));
+  console.log(parseFloat(data[0].longitude));
+  //to parse data from record
+  console.log((data[0].borough));
+
+  console.log(data[0].crash_date);
+  console.log((data[0].crash_time));
+
+  //street name variable
+  console.log(data[0].on_street_name);
+
+  //contributing factor
+  console.log(data[0].contributing_factor_vehicle_1);
+
+  //#of persons injured and killed
+  console.log(data[0].number_of_persons_injured);
+  console.log(data[0].number_of_persons_killed);
 });
