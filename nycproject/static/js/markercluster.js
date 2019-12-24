65171e1626687279e479c74de7280415a7d7d5e3
Miguel Patxot <51685818+MAPatxot@users.noreply.github.com>
2019-12-23 21:50:50 -0500

    Updated

--- nycproject/static/js/markercluster.js
+++ nycproject/static/js/markercluster.js
@@ -20,7 +20,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
 // var url = "/data/data.geojson"
 // Contains "null" values
 
-var url = "/data/cartodb-query.geojson"
+var url = "/nycproject/static/js/cartodb.geojson"
 
 d3.json(url).then( function(data) {
     console.log(data);
