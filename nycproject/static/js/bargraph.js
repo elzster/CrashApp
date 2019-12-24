2c1f62e134be5d3d0e0248713e48889795ea535b
elzster <hakim.elie1@gmail.com>
2019-12-23 23:39:14 -0500

    End of Day

--- nycproject/static/js/bargraph.js
+++ nycproject/static/js/bargraph.js
@@ -36,9 +36,9 @@ var nycData = `/datafile1/`;
 //Testing Table to HTML
 d3.json(nycData).then((data => {
 
-//   let unique = [...new Set(data.map(item => item.borough))];
-//   console.log(unique);
-    
+  let unique = [...new Set(data.map(item => item.borough))];
+  console.log(unique);
+  console.log(unique.length + " of Unique Boroughs in Dataset")  
   var filteredData = data.filter(d => d.borough === "Brooklyn");
   console.log(filteredData.length + " Crashes in "+ filteredData[0].borough);
 
@@ -54,9 +54,10 @@ d3.json(nycData).then((data => {
   var filteredData5 = data.filter(d => d.borough === "Staten Island");
   console.log(filteredData5.length + " Crashes in "+ filteredData5[0].borough);
 
-  //   //finds unique values in dataSet using map
-//   let unique1 = [...new Set(filteredData.map(item => item.on_street_name))];
-//   console.log(unique1);
+    //finds unique values in dataSet using map
+  let unique1 = [...new Set(filteredData.map(item => item.on_street_name))];
+  console.log(unique1.length + " Number of Different Streets in " + filteredData[0].borough);
+}));
 //   // var borough1 ={}
 //   // filteredData.reduce()
 
@@ -71,10 +72,64 @@ d3.json(nycData).then((data => {
   //     }
   //     // increment the count based on the type
   //     counts[o.type] += 1;
-  }));
+
 
 //   console.log(counts);
 // }));
+
+//#################BAR Graph################
+
+d3.json(nycData).then( data =>{
+  var filteredData = data.filter(d => d.borough === "Brooklyn" || "Staten Island" || "Queens" || "Manhattan" || "Bronx");
+  
+  // console.log(filteredData);
+
+  var trace1 = {
+    //good, Maps Borough on X Axis
+    x: data.map(d => d.borough),
+
+    y: data.map(d => d.id), 
+    
+    text: (data.length),
+
+    name: "Brooklyn",
+    type: "bar",
+    orientation: "v"
+  };
+
+  // set up the data variable
+  var data = [trace1]
+  // set up the layout variable
+  var layout = {
+    title: "Accidents by Borough",
+  }
+
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot('plot', data, layout);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //################Table######################
 //   d3.select("tbody").selectAll('tr')
