e552f15ee2a3baf6e9ebfd9aee9127cdc73e6fca
elzster <hakim.elie1@gmail.com>
2019-12-24 20:32:11 -0500

    Routes for Dynamic Boroughs Programmed

--- nycproject/static/js/bargraph.js
+++ nycproject/static/js/bargraph.js
@@ -110,9 +110,55 @@ d3.json(nycData).then( data =>{
 
 
 
+//Taken from Activity 1 Day 2 Week 15 - Fashions DropDown Menus for plotly.
 
+// function init() {
+//   data = [{
+//     x: [1, 2, 3, 4, 5],
+//     y: [0, 0, 0, 0, 0] }];
+//   var LINE = document.getElementById("plot");
+//   Plotly.plot(LINE, data);
+// }
 
+// function updatePlotly(newx, newy) {
+//   var LINE = document.getElementById("plot");
 
+//   // Note the extra brackets around 'newx' and 'newy'
+//   Plotly.restyle(LINE, "x", [newx]);
+//   Plotly.restyle(LINE, "y", [newy]);
+// }
+// //this will call different dataset switch variables which have different traces.
+// function getData(dataset) {
+
+//   // Initialize empty arrays to contain our axes
+//   var x = [];
+//   var y = [];
+
+//   //This Will Populate different instances in drop down.
+//   // Fill the x and y arrays as a function of the selected dataset
+//   switch (dataset) {
+//   case "dataset1":
+//     x = [1, 2, 3, 4, 5];
+//     y = [0.1, 0.2, 0.3, 0.4, 0.5];
+//     break;
+//   case "dataset2":
+//     x = [10, 20, 30, 40, 50];
+//     y = [1, 10, 100, 1000, 10000];
+//     break;
+//   case "dataset3":
+//     x = [100, 200, 300, 400, 500];
+//     y = [10, 100, 50, 10, 0];
+//     break;
+//   default:
+//     x = [1, 2, 3, 4, 5];
+//     y = [0, 0, 0, 0, 0];
+//     break;
+//   }
+//   //updates plotly plot.
+//   updatePlotly(x, y);
+// }
+
+// init();
 
 
 
